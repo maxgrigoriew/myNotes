d406f7532d253b1e04027892fbda88c3fc674c93
Max <sss123123111@yandex.ru>
2023-11-28 14:33:17 +0300

    init project

---
+++ src/components/IsHeader.vue
@@ -0,0 +1,37 @@
+<script lang="ts">
+import IsButton from "@/components/IsButton.vue";
+export default {
+  components: {
+    IsButton,
+  },
+};
+</script>
+
+<template>
+  <header class="header">
+    <div class="container">
+      <div class="header__inner">
+        <a href="#" class="logo">
+          <img
+            src="./../assets/images/logo.svg"
+            alt="Логотип"
+            width="218px"
+            height="50px"
+          />
+        </a>
+        <is-button></is-button>
+      </div>
+    </div>
+  </header>
+</template>
+
+<style lang="scss">
+.header {
+  &__inner {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 40px 0;
+  }
+}
+</style>
---
+++ src/views/IsHome.vue
@@ -0,0 +1,16 @@
+
+<script>
+import IsHeader from "@/components/IsHeader.vue";
+export default {
+  components: {
+    IsHeader,
+  },
+};
+</script>
+
+
+<template>
+  <div class="home">
+    <IsHeader />
+  </div>
+</template>
